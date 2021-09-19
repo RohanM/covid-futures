ca3017217d067e44827bba9c4cdb50bbd7fa5e63
Rohan Mitchell <rohan@rohanmitchell.com>
2021-09-19 10:45:17 +1000

    Display example chart

--- app/js/app.js
+++ app/js/app.js
@@ -1,21 +1,16 @@
-import * as rv from 'rough-viz/dist/roughviz.min'
+import { bb, line } from 'billboard.js';
+import "billboard.js/dist/billboard.css";
 
 window.onload = () => {
   const values = JSON.parse(document.getElementById('graph-vic').dataset.values);
 
-  new rv.Line({
-    element: '#graph-vic',
+  var chart = bb.generate({
+    bindto: "#graph-vic",
     data: {
-      cases: values,
+      type: line(),
+      columns: [
+        ["Cases"] + values,
+      ]
     },
-    width: window.innerWidth/2,
-    roughness: 0,
-    strokeWidth: 1,
-    circle: false,
-    interactive: false,
-    xLabel: 'Date',
-    yLabel: 'Cases',
-    legend: false,
-    title: 'Victoria',
   });
 };
