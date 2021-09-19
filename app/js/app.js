21772423c85f2c905a312ea538d536205d1222f3
Rohan Mitchell <rohan@rohanmitchell.com>
2021-09-19 10:49:25 +1000

    Display sample chart

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,10 @@
-console.log("Hello, JS!");
+import * as rv from 'rough-viz/dist/roughviz.min'
+
+window.onload = () => {
+  new rv.Bar({
+    element: '#viz0',
+    data: 'https://raw.githubusercontent.com/jwilber/random_data/master/flavors.csv',
+    labels: 'flavor',
+    values: 'price'
+  });
+};
