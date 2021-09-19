da3cafc3d4b39eea576415c8eb3a73766c58ca67
Rohan Mitchell <rohan@rohanmitchell.com>
2021-09-19 10:23:29 +1000

    Install css-loader and style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
 const path = require('path');
 
 module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+
   entry: './app/js/app.js',
   output: {
     filename: 'app.js',
     path: path.resolve(__dirname, 'app', 'static'),
   },
+
   devtool: 'eval-source-map',
   watch: true,
 };
