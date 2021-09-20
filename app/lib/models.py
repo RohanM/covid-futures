36a4b3f00abe45bd8aadfb02b85a9492e2c27c1e
Rohan Mitchell <rohan@rohanmitchell.com>
2021-09-20 19:09:10 +1000

    Set col length (reqd by mysql)

--- app/lib/models.py
+++ app/lib/models.py
@@ -3,7 +3,7 @@ from app import db
 class Case(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date, nullable=False)
-    state = db.Column(db.String, nullable=False)
+    state = db.Column(db.String(10), nullable=False)
     confirmed = db.Column(db.Integer)
 
     def __repr(self):
