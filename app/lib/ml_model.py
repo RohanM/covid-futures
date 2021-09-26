1d9c08136bf8e263fe357791ed14078bcc30bf6d
Rohan Mitchell <rohan@rohanmitchell.com>
2021-09-26 10:22:18 +1000

    Fit model

--- app/lib/ml_model.py
+++ app/lib/ml_model.py
@@ -1,25 +1,70 @@
+import torch
 from torch import nn, optim
 from app.lib.lambda_layer import Lambda
+from torch.optim.lr_scheduler import OneCycleLR
 
 class MLModel:
-    def __init__(self):
+    def __init__(self, input_window=30, output_window=30):
+        """
+        Parameters:
+        input_window: Size of the input data (in days)
+        output_window: Length of the prediction (in days)
+        """
         def add_channel(x): return x.unsqueeze(dim=1)
         def flatten(x): return x.view(x.shape[0], -1)
 
+        num_filters = 32
+
         self.__model = nn.Sequential(
             Lambda(add_channel),
-
-            nn.Conv1d(1,  32, 15, padding=7), nn.ReLU(),
-            nn.Conv1d(32, 32, 13, padding=6), nn.ReLU(),
-            nn.Conv1d(32, 32, 11, padding=5), nn.ReLU(),
-            nn.Conv1d(32, 32, 9,  padding=4), nn.ReLU(),
-            nn.Conv1d(32, 32, 7,  padding=3), nn.ReLU(),
-            nn.Conv1d(32, 32, 5,  padding=2), nn.ReLU(),
-            nn.Conv1d(32, 32, 3,  padding=1), nn.ReLU(),
-            nn.Conv1d(32, 32, 3,  padding=1), nn.ReLU(),
+            nn.Conv1d(          1, num_filters, 15, padding=7), nn.ReLU(),
+            nn.Conv1d(num_filters, num_filters, 13, padding=6), nn.ReLU(),
+            nn.Conv1d(num_filters, num_filters, 11, padding=5), nn.ReLU(),
+            nn.Conv1d(num_filters, num_filters, 9,  padding=4), nn.ReLU(),
+            nn.Conv1d(num_filters, num_filters, 7,  padding=3), nn.ReLU(),
+            nn.Conv1d(num_filters, num_filters, 5,  padding=2), nn.ReLU(),
+            nn.Conv1d(num_filters, num_filters, 3,  padding=1), nn.ReLU(),
+            nn.Conv1d(num_filters, num_filters, 3,  padding=1), nn.ReLU(),
 
             Lambda(flatten),
-            nn.Linear(960, 30),
+            nn.Linear(input_window*num_filters, output_window),
         )
         # Actual learning rate will be defined by the scheduler when fitting
         self.__opt = optim.SGD(self.__model.parameters(), lr=0.01)
+
+    def fit(self, epochs, dataloader_train, dataloader_valid):
+        train_losses, valid_losses = [], []
+        scheduler = OneCycleLR(self.__opt, 0.01, total_steps=epochs, steps_per_epoch=1)
+        loss_func = nn.MSELoss()
+
+        for epoch in range(epochs):
+            train_losses.append(
+                self.__train(dataloader_train, loss_func)
+            )
+            valid_losses.append(
+                self.__evaluate(dataloader_valid, loss_func)
+            )
+            scheduler.step()
+
+        return train_losses, valid_losses
+
+    def __train(self, dataloader, loss_func):
+        """Perform one training cycle and return the average loss."""
+        self.__model.train()
+        total_loss = 0.
+        for xb, yb in dataloader:
+            loss = loss_func(self.__model(xb), yb)
+            total_loss += loss
+            loss.backward()
+            self.__opt.step()
+            self.__opt.zero_grad()
+        return total_loss / len(dataloader)
+
+    def __evaluate(self, dataloader, loss_func):
+        self.__model.eval()
+        with torch.no_grad():
+            total_loss = 0.
+            for xb, yb in dataloader:
+                pred = self.__model(xb)
+                total_loss += loss_func(pred, yb)
+        return total_loss / len(dataloader)
--- tests/test_ml_model.py
+++ tests/test_ml_model.py
@@ -1,5 +1,13 @@
 import pytest
 from app.lib.ml_model import MLModel
+from app.lib.ml_model_data import MLModelData
 
-def test_init():
-    model = MLModel()
+def test_fit(app, db_extended_cases):
+    """Integration test of MLModelData with MLModel.fit()"""
+    with app.app_context():
+        data = MLModelData(input_window=2, output_window=2, train_valid_split=0.75)
+        data.load()
+        model = MLModel(input_window=2, output_window=2)
+        train_losses, valid_losses = model.fit(2, data.dataloader_train, data.dataloader_valid)
+        assert len(train_losses) == 2
+        assert len(valid_losses) == 2
