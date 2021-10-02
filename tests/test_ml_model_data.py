35c1c8aa85988eef19ee1fadc4625d6c8baf4b28
Rohan Mitchell <rohan@rohanmitchell.com>
2021-10-02 14:01:40 +1000

    Increase test fixture size

--- tests/test_ml_model_data.py
+++ tests/test_ml_model_data.py
@@ -8,15 +8,19 @@ def normalised_extended_cases():
     """Map values in db_extended_cases to normalised values"""
     return {
         10: pytest.approx(-0.92, abs=0.01),
-        20: pytest.approx(-0.86, abs=0.01),
-        30: pytest.approx(-0.80, abs=0.01),
-        40: pytest.approx(-0.74, abs=0.01),
-        50: pytest.approx(-0.68, abs=0.01),
-        100: pytest.approx(-0.39, abs=0.01),
-        200: pytest.approx(0.21, abs=0.01),
-        300: pytest.approx(0.80, abs=0.01),
-        400: pytest.approx(1.40, abs=0.01),
-        500: pytest.approx(1.99, abs=0.01),
+        20: pytest.approx(-0.87, abs=0.01),
+        30: pytest.approx(-0.83, abs=0.01),
+        40: pytest.approx(-0.78, abs=0.01),
+        50: pytest.approx(-0.74, abs=0.01),
+        60: pytest.approx(-0.70, abs=0.01),
+        70: pytest.approx(-0.65, abs=0.01),
+        100: pytest.approx(-0.52, abs=0.01),
+        200: pytest.approx(-0.09, abs=0.01),
+        300: pytest.approx(0.35, abs=0.01),
+        400: pytest.approx(0.78, abs=0.01),
+        500: pytest.approx(1.22, abs=0.01),
+        600: pytest.approx(1.66, abs=0.01),
+        700: pytest.approx(2.09, abs=0.01),
     }
 
 
@@ -43,6 +47,8 @@ def test_normalised_cases(app, db_extended_cases, normalised_extended_cases):
                 normalised_extended_cases[300],
                 normalised_extended_cases[400],
                 normalised_extended_cases[500],
+                normalised_extended_cases[600],
+                normalised_extended_cases[700],
             ],
             'VIC': [
                 normalised_extended_cases[10],
@@ -50,6 +56,8 @@ def test_normalised_cases(app, db_extended_cases, normalised_extended_cases):
                 normalised_extended_cases[30],
                 normalised_extended_cases[40],
                 normalised_extended_cases[50],
+                normalised_extended_cases[60],
+                normalised_extended_cases[70],
             ],
         }
 
@@ -61,44 +69,53 @@ def test_windowed_cases(app, db_extended_cases, normalised_extended_cases):
             'NSW': [
                 [normalised_extended_cases[100], normalised_extended_cases[200]],
                 [normalised_extended_cases[200], normalised_extended_cases[300]],
+                [normalised_extended_cases[300], normalised_extended_cases[400]],
+                [normalised_extended_cases[400], normalised_extended_cases[500]],
             ],
             'VIC': [
                 [normalised_extended_cases[10], normalised_extended_cases[20]],
                 [normalised_extended_cases[20], normalised_extended_cases[30]],
+                [normalised_extended_cases[30], normalised_extended_cases[40]],
+                [normalised_extended_cases[40], normalised_extended_cases[50]],
             ],
         }
         assert data.y == {
             'NSW': [
                 [normalised_extended_cases[300], normalised_extended_cases[400]],
                 [normalised_extended_cases[400], normalised_extended_cases[500]],
+                [normalised_extended_cases[500], normalised_extended_cases[600]],
+                [normalised_extended_cases[600], normalised_extended_cases[700]],
             ],
             'VIC': [
                 [normalised_extended_cases[30], normalised_extended_cases[40]],
                 [normalised_extended_cases[40], normalised_extended_cases[50]],
+                [normalised_extended_cases[50], normalised_extended_cases[60]],
+                [normalised_extended_cases[60], normalised_extended_cases[70]],
             ]
         }
 
 def test_split_train_validation(app, db_extended_cases):
     with app.app_context():
-        data = MLModelData(input_window=1, output_window=1, train_valid_split=0.75)
+        data = MLModelData(input_window=1, output_window=1, train_valid_split=0.7)
         data.load()
-        assert len(data.train_x['NSW']) == 3
-        assert len(data.train_x['VIC']) == 3
-        assert len(data.train_y['NSW']) == 3
-        assert len(data.train_y['VIC']) == 3
-        assert len(data.valid_x['NSW']) == 1
-        assert len(data.valid_x['VIC']) == 1
-        assert len(data.valid_y['NSW']) == 1
-        assert len(data.valid_y['VIC']) == 1
+        print(data.x)
+        assert len(data.train_x['NSW']) == 4
+        assert len(data.train_x['VIC']) == 4
+        assert len(data.train_y['NSW']) == 4
+        assert len(data.train_y['VIC']) == 4
+        assert len(data.valid_x['NSW']) == 2
+        assert len(data.valid_x['VIC']) == 2
+        assert len(data.valid_y['NSW']) == 2
+        assert len(data.valid_y['VIC']) == 2
 
 def test_combine_data(app, db_extended_cases):
     with app.app_context():
-        data = MLModelData(input_window=1, output_window=1, train_valid_split=0.75)
+        data = MLModelData(input_window=1, output_window=1, train_valid_split=0.7)
         data.load()
-        assert len(data.all_train.x) == 6
-        assert len(data.all_train.y) == 6
-        assert len(data.all_valid.x) == 2
-        assert len(data.all_valid.y) == 2
+        assert len(data.all_train.x) == 8
+        assert len(data.all_train.y) == 8
+        assert len(data.all_valid.x) == 4
+        assert len(data.all_valid.y) == 4
 
 def test_dataloader(app, db_extended_cases):
     with app.app_context():
