83faee705bea9c2122a79daea1770a3d9c933cd7
Rohan Mitchell <rohan@rohanmitchell.com>
2021-10-05 17:19:22 +1100

    Fix off-by-one error when making prediction

--- bin/predict.py
+++ bin/predict.py
@@ -16,7 +16,7 @@ with create_app().app_context():
     model.load('./model.pt')
 
     for state in Case.states():
-        prediction_date = Case.latest_date(state)
+        prediction_date = Case.latest_date(state) + datetime.timedelta(days=1)
         recent_cases = data.normalised_cases[state][-input_window:]
         prediction_data = model.predict(recent_cases).int().tolist()
         Prediction.save(state, prediction_date, prediction_data)
